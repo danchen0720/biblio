1313c78e24fafccd7a7130d88648dcf24569c724
danchen0720 <danchen@potix.com>
2013-11-21 16:59:13 +0800

    Database and Hibernate connected great,and tested

---
+++ src/main/java/com/danchen/biblio/test/TestDAO.java
@@ -0,0 +1,18 @@
+package com.danchen.biblio.test;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.danchen.biblio.misc.HibernateUtil;
+
+public class TestDAO {
+	public static void main(String args[]){
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction tx = session.beginTransaction();
+		Query query = session.createQuery("from Tag");
+		System.out.println(query.list().size());
+		tx.commit();
+		HibernateUtil.closeSessionFactory();
+	}
+}
