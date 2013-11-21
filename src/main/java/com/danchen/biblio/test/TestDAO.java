54db0b6c89421d3e17a026f06a04685cef689125
danchen0720 <danchen@potix.com>
2013-11-21 18:56:32 +0800

    update Aritcle.content type

--- src/main/java/com/danchen/biblio/test/TestDAO.java
+++ src/main/java/com/danchen/biblio/test/TestDAO.java
@@ -1,5 +1,6 @@
 package com.danchen.biblio.test;
 
+import org.apache.log4j.Logger;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -7,6 +8,7 @@ import org.hibernate.Transaction;
 import com.danchen.biblio.misc.HibernateUtil;
 
 public class TestDAO {
+	
 	public static void main(String args[]){
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		Transaction tx = session.beginTransaction();
